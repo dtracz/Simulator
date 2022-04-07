c3875a3db64433afca79a0d5270373f5c55e3b84
Dawid Tracz <dawid.b.tracz@gmail.com>
2022-04-07 02:11:12 +0200

    add Infrastructure

--- Machine.py
+++ Machine.py
@@ -1,9 +1,33 @@
 from Resource import *
 
 
+class Infrastructure(dict):
+    __self = None
+
+    def __init__(self):
+        if Infrastructure.__self != None:
+            raise Exception("Creating another instance of Infrastructure is forbidden")
+        self.machine = super()
+        Infrastructure.__self = self
+
+    @staticmethod
+    def getInstance(*args, **kwargs):
+        if Infrastructure.__self == None:
+            Infrastructure(*args, **kwargs)
+        return Simulator.__self;
+
+    def addMachine(self, machine):
+        self[machine.name] = machine
+
+    @property
+    def machines(self):
+        return list(self.values())
+
+
+
 class Machine:
     def __init__(self, name, resources):
-        self._name = name
+        self.name = name
         self._resources = resources
 
     def allocate(self, job):
