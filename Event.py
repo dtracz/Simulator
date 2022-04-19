69a898d48762a30dc49b63cead3e6ac773bc140e
Dawid Tracz <dawid.b.tracz@gmail.com>
2022-04-20 01:15:45 +0200

    make JobRecalcutate not inheriting over JobStart

--- Event.py
+++ Event.py
@@ -74,14 +74,24 @@ class JobStart(Event):
 
 
 
-class JobRecalculate(JobStart):
+class JobRecalculate(Event):
     """
     Job recalculation. Needs to be proceed when
     some resources of already running job change.
     """
     def __init__(self, job, priority=100):
-        super().__init__(job, priority)
-        self.name = f"JobRecalculate_{job.name}"
+        super().__init__(lambda: None, f"JobRecalculate_{job.name}", priority)
+        self.job = job
+        self._time = None
+
+    def scheduleFinish(self):
+        self._time = Simulator.getInstance().time
+        execTime = self.job.calculateExecTime()
+        endTime = self._time + execTime
+        jobFinish = JobFinish(self.job)
+        Simulator.getInstance().addEvent(endTime, jobFinish)
+        self.job.predictedFinish = jobFinish
+        self.job.update()
 
     def deletePrevFinish(self):
         jobFinish = self.job.predictedFinish
