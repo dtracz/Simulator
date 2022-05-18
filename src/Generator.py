8b2772e747bce4583ad8331c6e0b9150243400d5
Dawid Tracz <dawid.b.tracz@gmail.com>
2022-05-18 23:13:20 +0200

    add from json job generator

--- src/Generator.py
+++ src/Generator.py
@@ -1,4 +1,5 @@
 from numpy import random
+import json, string
 from Job import *
 from Resource import *
 from Machine import *
@@ -53,4 +54,49 @@ class CreateVM:
             req += [ResourceRequest(Resource.Type.CPU_core, float('inf'))]
         vm = VirtualMachine(name, req, scheduler)
         return vm
+
+
+
+class FromFileJobGenerator:
+    def __init__(self, fname):
+        self.fname = fname
+        self.cpuSpeeds = {'xeon e3-1270 v5': 3.6}
+        self.file = open(self.fname)
+        next(self.file)
+
+    @staticmethod
+    def createJob(operations, noCores, ramSize, machine=None):
+        req = [ResourceRequest(Resource.Type.RAM, ramSize)]
+        for _ in range(noCores):
+            req += [ResourceRequest(Resource.Type.CPU_core, float('inf'))]
+        return Job(operations, req, machine)
+
+    @staticmethod
+    def _randWord(length):
+        letters = string.ascii_lowercase
+        return ''.join([random.choice(list(letters)) for i in range(length)])
+
+    def parseLine(self, line):
+        tmp = ''
+        while tmp in line:
+            tmp = self._randWord(8)
+        line = line.replace('""', tmp)
+        parts = line.split('"')
+        for i, part in enumerate(parts):
+            parts[i] = part.replace(tmp, '"')
+        jobRequest = json.loads(parts[1])
+        jobReport = json.loads(parts[3])
+        cpuType = jobRequest['requires'][0].split(':')[1]
+        timeLimit = float(jobRequest['limits']['time'][:-1])
+        ops = self.cpuSpeeds[cpuType] * timeLimit
+        noCores = int(jobRequest['limits']['cpus'])
+        ramSize = int(jobRequest['limits']['memory'][:-1]) / 1e9
+        return ops, noCores, ramSize
+
+    def getJobs(self, n=1, machine=None):
+        for i in range(n):
+            line = next(self.file)
+            ops, noCores, ramSize = self.parseLine(line)
+            job = self.createJob(ops, noCores, ramSize, machine)
+            yield job
 
