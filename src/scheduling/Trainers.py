ff48b962c746bc97033a6ff33a857002a78e723a
Dawid Tracz <dawid.b.tracz@gmail.com>
2022-08-15 04:40:54 +0200

    add random trainer

---
+++ src/scheduling/Trainers.py
@@ -0,0 +1,53 @@
+import numpy as np
+
+
+class RandomTrainer:
+    def __init__(self, model, score_fun, epoch_size=100, n_bests=20,
+                 init_min=-1, init_max=1):
+        self.model = model
+        self.score_fun = score_fun
+        self._epoch_size = epoch_size
+        self._n_bests = n_bests
+        self._var_shapes = []
+        self._len_theta = 0
+        for var in model.getVars():
+            self._var_shapes += [var.shape]
+            self._len_theta += np.prod(var.shape)
+        self._thetas = np.random.uniform(init_min, init_max,
+                                         (epoch_size, self._len_theta))
+        self._scores = -np.ones(epoch_size)*float('inf')
+
+    def toVars(self, theta):
+        splitpoints = np.cumsum(self._var_shapes)[:-1]
+        varList = np.array_split(theta, splitpoints)
+        return varList
+
+    def scoreThetas(self):
+        for i in range(self._epoch_size):
+            varList = self.toVars(self._thetas[i])
+            self.model.setVars(varList)
+            score = self.score_fun(self.model)
+            self._scores[i] = score
+
+    def getBests(self, n_bests):
+        idc = np.argpartition(self._scores, -self._n_bests)[-self._n_bests:]
+        bests = self._theta[idc]
+        best_scores = self._scores[idc]
+        return bests, best_scores
+
+    def step(self):
+        self.scoreThetas()
+        bests, best_scores = self.getBests(self._n_bests)
+        self._thetas = np.random.normal(bests.mean(axis=0),
+                                        bests.std(axis=0),
+                                        (self._epoch_size, self._len_theta))
+        self._scores = -np.ones(epoch_size)*float('inf')
+        return best_scores.mean()
+
+    def train(self, n_steps):
+        progress_line = []
+        for _ in range(n_steps):
+            score = self.step()
+            progress_line += [score]
+        return progress_line
+
