aa4ad5ed7be411e0f61ef488f2ffd07bd31fbe68
Dawid Tracz <dawid.b.tracz@gmail.com>
2022-08-15 04:45:04 +0200

    add torch model

--- src/scheduling/Models.py
+++ src/scheduling/Models.py
@@ -1,4 +1,5 @@
 import numpy as np
+import torch
 
 
 class RandomModel:
@@ -59,5 +60,37 @@ class Model_v0_np:
         x = x @ self.fc1
         x = x[...,-1]
         x = self.softmax(x)
+        return x
+
+
+
+class Model_v0_torch(torch.nn.Module):
+    def __init__(self, machines_dims, task_dim, a_space):
+        super().__init__()
+        mdy, mdx = machines_dims
+        self.conv0 = torch.nn.Conv1d(mdx+task_dim, 32, 1, bias = False)
+        self.activ0 = torch.nn.LeakyReLU()
+        self.conv1 = torch.nn.Conv1d(32+task_dim, 1, 1, bias = False)
+        self.flat = torch.nn.Flatten()
+        self.sm = torch.nn.Softmax(dim=-1)
+
+    @staticmethod
+    def prepare(inp):
+        x, cat = inp
+        x = torch.Tensor(x).transpose(1,2) # NLC to NCL
+        cat = np.expand_dims(cat, 1).transpose(0,2,1)
+        task_dim = cat.shape[1]
+        cat = torch.Tensor(cat).expand(-1,task_dim,x.shape[-1])
+        return x, cat
+
+    def forward(self, inp):
+        x, cat = self.prepare(inp)
+        x = torch.cat((x, cat), 1)
+        x = self.conv0(x)
+        x = self.activ0(x)
+        x = torch.cat((x, cat), 1)
+        x = self.conv1(x)
+        x = self.flat(x)
+        x = self.sm(x)
         return x
 
